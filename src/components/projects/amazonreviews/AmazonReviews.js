1565657f4e0bb9a162aba9e90d7ff2cfd5915c1a
devme619 <devme619@gmail.com>
2022-08-25 01:29:17 +0530

    basic

--- src/components/projects/amazonreviews/AmazonReviews.js
+++ src/components/projects/amazonreviews/AmazonReviews.js
@@ -1,6 +1,6 @@
 import React from "react";
-import './AmazonReviews.css'
-import GitHubIcon from '@mui/icons-material/GitHub';
+import "./AmazonReviews.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
 const AmazonReviews = () => {
   return (
     <div className="amazonreviews">
@@ -10,12 +10,12 @@ const AmazonReviews = () => {
         </h5>
         <p>
           Collected open source amazon reviews dataset of 20K reviews of
-          category like Watches, Shoes, Health, Phones, Arts. Used the packages
-          like VADER to get the best labels for the reviews and created Randon
-          Forest model and got the accuracy around 76 percent.
+          categories like Watches, Shoes, Health, Phones, and Arts. Used the
+          packages VADER to get the best labels for the reviews and created the
+          Random Forest model and got an accuracy of around 76 percent.
         </p>
         <a target="blank" href="https://github.com/devme619/ML--Amazon-Reviews">
-          <GitHubIcon/>
+          <GitHubIcon />
         </a>
       </div>
     </div>
--- src/components/projects/movierevenue/MovieRevenue.js
+++ src/components/projects/movierevenue/MovieRevenue.js
@@ -1,15 +1,15 @@
 import React from "react";
-import './MovieRevenue.css'
+import "./MovieRevenue.css";
 const MovieRevenue = () => {
   return (
     <div className="movierevenue">
       <div className="content">
         <h5 className="heading-movierevenue">Movie Revenue Prediction</h5>
         <p>
-          Worked on the TMDB box office dataset to predict the movie revenue.
-          Performed the exploratory data analysis and data preprocessing to find
-          the best feature. Created models like linear regression,decision tree
-          and achieved the accuracy of 90 percent using the random forest model.
+          Performed exploratory data analysis and data preprocessing on the TMDB
+          box office dataset to find the best feature. Created models like
+          Linear Regression, and Decision Tree and achieved the accuracy of 90
+          percent using the Random forest model.
         </p>
       </div>
     </div>
