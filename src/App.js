502017db96ab9f022f2e9b6c082a5cc2d8a0c4a6
devme619 <devme619@gmail.com>
2026-06-21 02:04:25 +0530

    removed extra files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Header from './components/header/Header';
-import About from './components/projects/about/About';
-import Project from './components/projects/Project';
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Header from "./components/header/Header";
+import About from "./components/projects/about/About";
+import Project from "./components/projects/Project";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <About/>
-      <Experience/>
-      <Project/>
-      <Contact/>
+    <div>
+      <Header />
+      <About />
+      <Experience />
+      <Project />
+      <Contact />
     </div>
   );
 }
